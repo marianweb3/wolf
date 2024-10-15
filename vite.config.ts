480f936ba643440aa104f70b485564754d5e1f6d
OlehOrenchuk <oleh.orenchuk@gmail.com>
2024-10-15 19:22:54 +0300

    vite config edit for vercel prod

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      crypto: "crypto-browserify",
+      // crypto: "crypto-browserify",
       "@": path.resolve(__dirname, "./src"),
     },
   },
